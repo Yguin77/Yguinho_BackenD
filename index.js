47e04786a5e4487dfd0f265c0081da187a31c78d
Yguin77 <fariasygor234@gmail.com>
2025-08-21 19:30:57 -0300

    Movendo arquivo JS

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "Ygor"
+
+let sobrenome = "Farias"
+
+console.log (nome + " " + sobrenome)
+
+let idade = 23
+ console.log("A idade do " + nome + " é " + idade)
+
+ let exerecicios = 0.5
+
+ let trabalho = 1.5
+
+ let prova = 4.5
+
+ let notaA1Calculada = exerecicios + trabalho + prova
+
+ console.log("NOTA A1: " + notaA1Calculada)
